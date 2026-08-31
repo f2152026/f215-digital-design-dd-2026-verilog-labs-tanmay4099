1c2061bc0106262702c51a2afb3e203946fddf94
Tanmay4099 <f20250499@pilani.bits-pilani.ac.in>
2026-08-31 12:56:03 +0000

    lab01 task3

--- labs/lab01/task3/cla4_dataflow.v
+++ labs/lab01/task3/cla4_dataflow.v
@@ -26,4 +26,23 @@ module cla4_dataflow(
 
   // TODO: your dataflow (assign) statements go here.
 
+  // Propagate and Generate
+    assign p = a ^ b;
+    assign g = a & b;
+
+  // Carry Look-Ahead equations
+    assign c1 = g[0] | (p[0] & cin);
+
+    assign c2 = g[1] | (p[1] & g[0]) | (p[1] & p[0] & cin);
+
+    assign c3 = g[2] | (p[2] & g[1]) | (p[2] & p[1] & g[0]) | (p[2] & p[1] & p[0] & cin);
+
+    assign cout = g[3] | (p[3] & g[2]) | (p[3] & p[2] & g[1]) | (p[3] & p[2] & p[1] & g[0]) | (p[3] & p[2] & p[1] & p[0] & cin);
+
+  // Sum
+    assign sum[0] = p[0] ^ cin;
+    assign sum[1] = p[1] ^ c1;
+    assign sum[2] = p[2] ^ c2;
+    assign sum[3] = p[3] ^ c3;
+
 endmodule
